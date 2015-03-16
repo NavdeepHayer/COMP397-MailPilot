c51306eac581fc014e642bcf1015daefc716a726
tsiliopoulos <tsiliopoulos@hotmail.com>
2015-03-16 18:25:04 -0400

    Added Menu State & Game Over State

--- COMP397-MailPilot/Scripts/game.js
+++ COMP397-MailPilot/Scripts/game.js
@@ -4,24 +4,39 @@
 /// <reference path="typings/soundjs/soundjs.d.ts" />
 /// <reference path="typings/preloadjs/preloadjs.d.ts" />
 /// <reference path="typings/stats/stats.d.ts" />
+/// <reference path="constants.ts" />
 /// <reference path="objects/gameobject.ts" />
 /// <reference path="objects/scoreboard.ts" />
 /// <reference path="objects/plane.ts" />
 /// <reference path="objects/island.ts" />
 /// <reference path="objects/cloud.ts" />
 /// <reference path="objects/ocean.ts" />
+/// <reference path="objects/button.ts" />
+/// <reference path="objects/label.ts" />
 /// <reference path="states/gameplay.ts" />
+/// <reference path="states/gameover.ts" />
+/// <reference path="states/menu.ts" />
 // Global game Variables
 var canvas;
 var stage;
 var assetLoader;
 var stats = new Stats();
+var currentScore = 0;
+var highScore = 0;
+// Game State Variables
+var currentState;
+var currentStateFunction;
+var stateChanged = false;
 var gamePlay;
+var gameOver;
+var menu;
 var manifest = [
     { id: "cloud", src: "assets/images/cloud.png" },
     { id: "island", src: "assets/images/island.png" },
     { id: "ocean", src: "assets/images/ocean.gif" },
     { id: "plane", src: "assets/images/plane.png" },
+    { id: "playButton", src: "assets/images/playButton.png" },
+    { id: "tryAgainButton", src: "assets/images/tryAgainButton.png" },
     { id: "engine", src: "assets/audio/engine.ogg" },
     { id: "yay", src: "assets/audio/yay.ogg" },
     { id: "thunder", src: "assets/audio/thunder.ogg" }
@@ -39,7 +54,8 @@ function init() {
     createjs.Ticker.setFPS(60); // 60 frames per second
     createjs.Ticker.addEventListener("tick", gameLoop);
     setupStats();
-    main();
+    currentState = constants.MENU_STATE;
+    changeState(currentState);
 }
 function setupStats() {
     stats.setMode(0);
@@ -51,11 +67,32 @@ function setupStats() {
 }
 function gameLoop() {
     stats.begin();
-    gamePlay.update();
+    if (stateChanged) {
+        changeState(currentState);
+        stateChanged = false;
+    }
+    else {
+        currentStateFunction.update();
+    }
     stats.end();
 }
-function main() {
-    // Instantiate new Game State
-    gamePlay = new states.GamePlay();
+function changeState(state) {
+    switch (state) {
+        case constants.MENU_STATE:
+            // instantiate menu screen
+            menu = new states.Menu();
+            currentStateFunction = menu;
+            break;
+        case constants.PLAY_STATE:
+            // instantiate game play screen
+            gamePlay = new states.GamePlay();
+            currentStateFunction = gamePlay;
+            break;
+        case constants.GAME_OVER_STATE:
+            // instantiate game over screen
+            gameOver = new states.GameOver();
+            currentStateFunction = gameOver;
+            break;
+    }
 }
 //# sourceMappingURL=game.js.map
